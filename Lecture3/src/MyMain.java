f2ce3a97cac48f3a915391bf0db58b512dcf2e2a
Albert Morales <albert.morales@upr.edu>
2016-08-30 14:44:54 -0400

    Otro Cambio

--- Lecture3/src/MyMain.java
+++ Lecture3/src/MyMain.java
@@ -11,7 +11,7 @@ public class MyMain
 		frame.add(theComponent);
 		
 		frame.setSize(300, 400);
-		frame.setTitle("Java Guruhgjgs");
+		frame.setTitle("-----Java Gurus------");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
